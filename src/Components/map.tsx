2bb3f9bb693cbaaaa591511e62886c47e98f0477
Bmack1915 <119226403+Bmack1915@users.noreply.github.com>
2024-04-01 11:58:00 +0100

    Homepage and Map added
    Components HomePage and Map added for proof of concept. Basic insertion of components into App component. Minor css styling added

--- src/Components/map.tsx
+++ src/Components/map.tsx
@@ -1,12 +1,42 @@
 import React from "react";
-import { APIProvider, Map as GoogleMap } from "@vis.gl/react-google-maps";
+import { useState } from "react";
 
-const GOOGLE_MAPS_API_KEY = "AIzaSyD3_F4A1TSlJUAiLV7y7msB7fFW6NyqZ-s";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
 
-const CustomMap = () => (
-  <APIProvider apiKey={GOOGLE_MAPS_API_KEY}>
-    <GoogleMap zoom={10} center={{ lat: 53.54992, lng: 10.00678 }} />
-  </APIProvider>
-);
+export default function CustomMap() {
+  const position = { lat: 53.54, lng: 10 };
+  const [open, setOpen] = useState(false);
 
-export default CustomMap;
+  return (
+    <APIProvider apiKey={"AIzaSyDMBA9gmhjNuaXWW3T71okAtXIlYwTpgyU"}>
+      <div style={{ height: "40vh", width: "40%" }}>
+        <Map
+          zoom={12}
+          center={position}
+          zoomControl={true}
+          mapId={process.env.NEXT_PUBLIC_MAP_ID}
+        >
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin
+              background={"grey"}
+              borderColor={"green"}
+              glyphColor={"purple"}
+            />
+          </AdvancedMarker>
+
+          {open && (
+            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+              <p>I'm in Hamburg</p>
+            </InfoWindow>
+          )}
+        </Map>
+      </div>
+    </APIProvider>
+  );
+}
