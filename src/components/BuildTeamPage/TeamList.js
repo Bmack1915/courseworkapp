fde18054ef94d264f18dc3caa04731c6c64458f3
Bmack1915 <119226403+Bmack1915@users.noreply.github.com>
2024-04-23 12:51:46 +0100

    restyling of teamlist page

--- src/components/BuildTeamPage/TeamList.js
+++ src/components/BuildTeamPage/TeamList.js
@@ -180,10 +180,43 @@ const TeamList = () => {
 
   return (
     <AuthCheck>
-      <div className="container">
-        <div className="row">
-          <p className="mb-3">Select your team and start building</p>
+      <section class="py-5 bg-dark" id="features">
+        <div class="container px-5 my-5">
+          <div class="row gx-5">
+            <div class="col-lg-4 mb-5 mb-lg-0">
+              <h2 class="fw-bolder mb-0 text-white">
+                A better way to start building.
+              </h2>
+            </div>
+            <div class="col-lg-8">
+              <div class="row gx-5 row-cols-1 row-cols-md-2">
+                <div class="col mb-5 h-100">
+                  <div class="feature bg-dark bg-gradient text-white rounded-3 mb-3">
+                    <i class="bi bi-collection"></i>
+                  </div>
+                  <h2 class="h5 text-white">Featured title</h2>
+                  <p class="mb-0 text-white">
+                    Paragraph of text beneath the heading to explain the
+                    heading. Here is just a bit more text.
+                  </p>
+                </div>
+                <div class="col mb-5 h-100">
+                  <div class="feature bg-dark bg-gradient text-white rounded-3 mb-3">
+                    <i class="bi bi-toggles2"></i>
+                  </div>
+                  <h2 class="h5 text-white">Featured title</h2>
+                  <p class="mb-0 text-white">
+                    Paragraph of text beneath the heading to explain the
+                    heading. Here is just a bit more text.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </section>
+      <div className="container">
+        <div className="row"></div>
         <div className="row">
           <div className="col-md-4">
             <select
