522b90a873354f09d080e5c953d588dcf447b56b
Bmack1915 <119226403+Bmack1915@users.noreply.github.com>
2024-04-25 07:47:24 +0100

    Unneeded images removed

--- backend/obj/Debug/net8.0/WebCoursework.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/WebCoursework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCoursework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b711abfb4a7fe780f83828ba9f55fb55b1b9ce74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ec9da8848e12d8bf4ad90d01882c8c46ec0d5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCoursework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCoursework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
